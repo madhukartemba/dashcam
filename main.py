4b4c2069488e272fb0df8674247fe47ccfa546fe
madhukar.temba <madhukar.temba@arcadia.com>
2024-03-06 23:45:26 +0530

    create logs folder if it does not exist

--- main.py
+++ main.py
@@ -16,9 +16,14 @@ OUTPUT_FOLDER = "recordings"
 FILE_DURATION = 120
 
 # Logging
-LOG_DIR = "logs"
-logging.basicConfig(filename=os.path.join(LOG_DIR, 'errors.log'), level=logging.ERROR,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+LOGS_FOLDER = "logs"
+if not os.path.exists(LOGS_FOLDER):
+    os.makedirs(LOGS_FOLDER)
+logging.basicConfig(
+    filename=os.path.join(LOGS_FOLDER, "errors.log"),
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 # Input Source
 FPS = 30.0
