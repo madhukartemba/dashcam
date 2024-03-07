441382fdd8f8ee61ddc8a2b193c8f1e0a3c37403
madhukar.temba <madhukar.temba@arcadia.com>
2024-03-07 22:19:33 +0530

    test

--- flask_test.py
+++ flask_test.py
@@ -1,12 +1,11 @@
-from flask import Flask, app
+from flask import Flask
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
-
+# Make the app accessible across the network
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
