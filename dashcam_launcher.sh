2a82eb4309c72e0484d4957f0413ef0a7f585444
madhukartemba <madhusmiles.madhukar@gmail.com>
2024-03-10 23:05:28 +0530

    change max fps to 2 in launcher

--- dashcam_launcher.sh
+++ dashcam_launcher.sh
@@ -6,4 +6,4 @@ cd /home/madhukar/Desktop/dashcam
 # Pull the latest changes from the repository
 git pull || true
 
-lxterminal --command="python main.py --maxFps=3.0 --showPreview=false"
+lxterminal --command="python main.py --maxFps=2.0 --showPreview=false"
