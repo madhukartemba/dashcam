1d78084ea45f8cd35cce8e9ac4432f454a07b6f7
madhukartemba <madhusmiles.madhukar@gmail.com>
2024-03-12 14:48:50 +0530

    remove delay in launcher

--- dashcam_launcher.sh
+++ dashcam_launcher.sh
@@ -2,12 +2,11 @@
 # dashcam_launcher.sh
 
 sudo systemctl stop isc-dhcp-server
+sudo systemctl start isc-dhcp-server
 
 cd /home/madhukar/Desktop/dashcam
 
 # Pull the latest changes from the repository
 git pull || true
-
-sudo systemctl start isc-dhcp-server
 
 lxterminal --command="python main.py --maxFps=2.1"
