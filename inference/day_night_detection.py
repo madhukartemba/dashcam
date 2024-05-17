b191e4cd1aec056a2c60f4dd2dc7d6df04cf4af3
madhukar.temba <madhukar.temba@arcadia.com>
2024-05-17 22:51:11 +0530

    day_night_detection

---
+++ inference/day_night_detection.py
@@ -0,0 +1,36 @@
+import logging
+import threading
+from input_output.input_source import InputSource
+
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
+
+
+class DayNightDetection:
+    def __init__(self, inputSource: InputSource, thresholdBrightness=50):
+        self.thread = None
+        self.inputSource = inputSource
+        self.stopEvent = threading.Event()
+        self.darkMode = True
+        self.thresholdBrightness = thresholdBrightness
+        pass
+
+    def detect(self):
+        try:
+            while not self.stopEvent.is_set():
+                image = self.inputSource.getImage()
+                grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                averageBrightness = np.mean(grayImage)
+                self.darkMode = averageBrightness < self.thresholdBrightness
+        except Exception as e:
+            print(e)
+            logging.error(e)
+            self.stopEvent.set()
+
+    def start(self):
+        self.thread = threading.Thread(target=self.detect)
+
+    def stop(self):
+        self.stopEvent.set()
+        if self.thread:
+            self.thread.join()
