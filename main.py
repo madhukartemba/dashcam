a949c0bd7e75f925d85493ade441035c60f009c8
Madhukar Temba <madhusmiles.madhukar@gmail.com>
2024-05-25 23:20:55 +0530

    reduce size to 32 gb

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from inference.inference import Inference
 # File config
 RECOVERY_FOLDER = "recovery"
 OUTPUT_FOLDER = "recordings"
-MAX_FOLDER_SIZE_BYTES = 48 * 1024 * 1024 * 1024  # 48GB
+MAX_FOLDER_SIZE_BYTES = 32 * 1024 * 1024 * 1024  # 32GB
 FILE_DURATION = 10 * 60  # 10 Minutes
 
 # Logging
