e59d339e20022eeda7216cb80e156f7adcfbb4f4
Madhukar Temba <madhusmiles.madhukar@gmail.com>
2024-06-01 16:49:04 +0530

    update launcher script

--- dashcam_launcher.sh
+++ dashcam_launcher.sh
@@ -3,7 +3,11 @@
 
 cd /home/madhukar/Desktop/dashcam
 
-# Pull the latest changes from the repository
+# Pull the latest changes from the repository in the current terminal
 git pull || true
 
+# Open a new lxterminal and run git pull after a 30-second delay
+lxterminal --command="bash -c 'sleep 30; git pull || true'"
+
+# Start the Python script in a new lxterminal
 lxterminal --command="python main.py --maxFps=3.0"
