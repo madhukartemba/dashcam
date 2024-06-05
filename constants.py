ea0d69b65fb8bf6117f691ef59d319234cad74d5
madhukartemba <madhusmiles.madhukar@gmail.com>
2024-06-05 22:38:14 +0530

    decrease the file duration to 3 minutes

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
 # Software version
-VERSION = '1.0'
+VERSION = '1.1'
 
 # File config
 RECOVERY_FOLDER = "recovery"
 OUTPUT_FOLDER = "recordings"
 CACHE_FOLDER = "cache"
 MAX_FOLDER_SIZE_BYTES = 32 * 1024 * 1024 * 1024  # 32GB
-FILE_DURATION = 10 * 60  # 10 Minutes
+FILE_DURATION = 3 * 60  # 3 Minutes
 
 # Logging
 LOGS_FOLDER = "logs"
