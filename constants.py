16a9565283c308dcc6769e86b625147126508481
madhukartemba <madhusmiles.madhukar@gmail.com>
2024-06-08 16:42:11 +0530

    increase file duration

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ RECOVERY_FOLDER = "recovery"
 OUTPUT_FOLDER = "recordings"
 CACHE_FOLDER = "cache"
 MAX_FOLDER_SIZE_BYTES = 32 * 1024 * 1024 * 1024  # 32GB
-FILE_DURATION = 3 * 60  # 3 Minutes
+FILE_DURATION = 10 * 60  # 10 Minutes
 
 # Logging
 LOGS_FOLDER = "logs"
